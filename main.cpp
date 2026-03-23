60873ae2600ca124a2ed4a11056990121bb9d9ed
WhyATent <WWyatnt@gmail.com>
2026-03-23 13:16:05 -0700

    a52 Done.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
-
+using namespace std;
 int main()
 {
-    // TODO
-
+    int num, cMax;
+    cout << "Insert 10 integers separated by spaces.\n";
+    for (int i=0; i<10; i++){
+        cin >> num;
+        if (i==0 or num>cMax){
+            cMax=num;
+        }
+    }
+    cout << "The max value is: " << cMax << endl;
     return 0;
 }
